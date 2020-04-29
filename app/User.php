0290935e1338025a902a9b312080447ad3c1ce45
RafaelRossales <rapha2704@gmail.com>
2020-04-29 20:19:37 -0300

    Fixing user model

--- app/User.php
+++ app/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function store()
+    {
+    	return $this->hasOne(Store::class);
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
